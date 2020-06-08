433939e0bfc40161862ba72c12a382194227716d
Shammi <shammi0107@gmail.com>
2020-06-08 09:13:27 +0530

    add all

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,54 +1,60 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	https://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
-*/
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'login/index';
+$route['changeInitialPassword'] = 'login/changeInitialPassword';
+
+$route['testing/(:any)'] = 'testing/$1';
+
+
+$route['admin'] = 'admins/index';
+$route['admin/addCourse'] = 'admins/addCourse';
+$route['admin/addTeacher'] = 'admins/addTeacher';
+$route['admin/addStudent'] = 'admins/addStudent';
+$route['admin/editStudent/(:any)'] = 'admins/editStudent/$1';
+$route['admin/editCourse/(:any)'] = 'admins/editCourse/$1';
+$route['admin/editTeacher/(:any)'] = 'admins/editTeacher/$1';
+$route['admin/viewStudent'] = 'admins/viewStudent';
+$route['admin/viewCourse'] = 'admins/viewCourse';
+$route['admin/viewTeacher'] = 'admins/viewTeacher';
+$route['admin/searchStudent'] = 'admins/searchStudent';
+$route['admin/searchCourse'] = 'admins/searchCourse';
+$route['admin/searchTeacher'] = 'admins/searchTeacher';
+$route['admin/changeStatus/(:any)/(:any)'] = 'admins/changeStatus/$1/$2';
+
+
+$route['teacher'] = 'teachers/index';
+$route['teacher/grade'] = 'teachers/grade';
+$route['teacher/courseDetails/(:any)'] = 'teachers/courseDetails/$1';
+$route['teacher/createAssignment/(:any)'] = 'teachers/createAssignment/$1';
+$route['teacher/editAssignment/(:any)/(:any)/(:any)'] = 'teachers/editAssignment/$1/$2/$3';
+$route['teacher/update/(:any)/(:any)/(:any)'] = 'teachers/update/$1/$2/$3';
+$route['teacher/viewAssignments/(:any)'] = 'teachers/viewAssignments/$1';
+$route['teacher/viewSubmissions/(:any)/(:any)/(:any)'] = 'teachers/viewSubmissions/$1/$2/$3';
+$route['teacher/viewSubmissions/(:any)/(:any)'] = 'teachers/viewSubmissions/$1/$2';
+$route['teacher/viewStudents/(:any)'] = 'teachers/viewStudents/$1';
+$route['teacher/profile'] = 'teachers/profile';
+$route['teacher/course/(:any)'] = 'Courses/index/$1';
+$route['teacher/assignment/create/(:any)'] = 'Assignments/create/$1';
+$route['teacher/viewIssues/(:any)'] = 'teachers/view_issues/$1';
+
+
+$route['student'] = 'students/index';
+$route['student/assignmentDetails/(:any)/(:any)/(:any)'] = 'students/assignmentDetails/$1/$2/$3';
+$route['student/assignmentDetails/(:any)/(:any)'] = 'students/assignmentDetails/$1/$2';
+$route['student/courseDetails/(:any)'] = 'students/courseDetails/$1';
+$route['student/enrollCourse/(:any)'] = 'students/enrollCourse/$1';
+$route['student/viewGrade/(:any)/(:any)'] = 'students/viewGrade/$1/$2';
+$route['student/viewCourses'] = 'students/viewCourses';
+$route['student/profile'] = 'students/profile';
+$route['student/(:any)'] = 'students/view/$1';
+$route['student/reportIssue/(:any)'] = 'students/report_issue/$1';
+$route['student/viewIssue/(:any)'] = 'students/view_issues/$1';
+$route['student/callapi'] = 'students/call_api';
+
+
+
+$route['compiler'] = 'compiler/index';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -4,7 +4,7 @@ class Login extends CI_Controller {
 
 	function index(){
 
-		$this->load->view('Login/Index');
+		$this->load->view('login/index');
 	}
 
 	function loginUser(){
