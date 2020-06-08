0fe58a8afe669230b7e7264ad1d67c1f1c660b7b
Shammi <shammi0107@gmail.com>
2020-06-08 10:37:15 +0530

    add all

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Login_model', 'assignment_model', 'teacher_model', 'student_model', 'Submission_model', 'issue_model', 'course_model', 'Testcase_model');
+$autoload['model'] = array('');
